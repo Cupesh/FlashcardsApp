c88b936b2e8ac71c8980aee1629ed9c4c54bba20
Martin Cupak <cupak.martin90@gmail.com>
2021-03-11 22:22:29 +0000

    add swagger versioning
    mock controller

---
+++ FlashcardsAppDataManager/Controllers/V1/FlashcardController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlashcardsAppDataManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlashcardsAppDataManager.Controllers
+{
+    public class FlashcardController : Controller
+    {
+        private List<Flashcard> _flashcards;
+        public FlashcardController()
+        {
+            _flashcards = new List<Flashcard>();
+            _flashcards.Add(new Flashcard { Id = 1, Term = "a", Definition = "aa" });
+            _flashcards.Add(new Flashcard { Id = 1, Term = "b", Definition = "bb" });
+        }
+
+        [HttpGet("api/v1/flashcards")]
+        public IActionResult GetAll()
+        {
+            return Ok(_flashcards);
+        }
+    }
+}
---
+++ FlashcardsAppDataManager/Models/Flashcard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlashcardsAppDataManager.Models
+{
+    public class Flashcard
+    {
+        public int Id { get; set; }
+        public string Term { get; set; }
+        public string Definition { get; set; }
+    }
+}
