8b02bc804d9e92a7f5f27aeef9c14bf59bce3e4b
Martin Cupak <cupak.martin90@gmail.com>
2021-03-13 00:50:40 +0000

    Adding MVVM pattern to Xamarin.Forms

---
+++ FlashcardsApp/FlashcardsApp/Models/Flashcard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlashcardsApp.Models
+{
+    public class Flashcard
+    {
+        public int Id { get; set; }
+        public string Term { get; set; }
+        public string Definition { get; set; }
+    }
+}
