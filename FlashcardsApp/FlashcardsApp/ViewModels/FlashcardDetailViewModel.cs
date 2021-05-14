50342a5c1a01e7c29dd8f3ec27573a7d710c92a6
Cupesh <cupak.martin90@gmail.com>
2021-05-14 23:13:53 +0100

    carousel page binding
    null reference exception bug needs resolving

---
+++ FlashcardsApp/FlashcardsApp/ViewModels/FlashcardDetailViewModel.cs
@@ -0,0 +1,18 @@
+﻿using FlashcardsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FlashcardsApp.ViewModels
+{
+    public class FlashcardDetailViewModel : ViewModel
+    {
+        public Flashcard Flashcard { get; set; }
+
+        public FlashcardDetailViewModel(Flashcard flashcard)
+        {
+            Flashcard = flashcard;
+        }
+    }
+}
--- FlashcardsApp/FlashcardsApp/ViewModels/FlashcardsViewModel.cs
+++ FlashcardsApp/FlashcardsApp/ViewModels/FlashcardsViewModel.cs
@@ -3,17 +3,21 @@ using FlashcardsApp.Views;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
+using Xamarin.Forms;
 
 namespace FlashcardsApp.ViewModels
 {
     public class FlashcardsViewModel : ViewModel
     {
         public ObservableCollection<Flashcard> Flashcards { get; set; }
+        public ObservableCollection<FlashcardDetailViewModel> Pages { get; set; }
 
         public void Initialize(ObservableCollection<Flashcard> flashcards)
         {
             Flashcards = flashcards;
+            Pages = new ObservableCollection<FlashcardDetailViewModel>(Flashcards.Select(f => new FlashcardDetailViewModel(f)));
         }
     }
 }
